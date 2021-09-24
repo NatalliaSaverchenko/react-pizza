483e26b6de3e1a7c8b7bfb41fec7f439b1aeecf6
NatalliaSaverchenko <saverchenko.nn@gmail.com>
2021-09-24 15:50:18 +0300

    create components: header, button, categories

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import classNames from "classnames"
+
+const Button = ({ outline, onClick, className, children }) => {
+    return (
+        <button 
+        className={classNames('button', className, {'button--outline': outline})}
+        onClick={onClick}        >
+            {children}
+        </button>
+    )
+}
+export default Button 
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// import Header from './Header/Header';
+// import Button from './Button/Button';
+// import Categories from './Categories/Categories';
+
+// export { Header, Button, Categories} 
+export { default as Button } from './Button/Button'
+export { default as Header } from './Header/Header'
+export { default as Categories } from './Categories/Categories'
